578397dd3424b10101aa7a3e2e872636c4427359
Alexander Lerma <alexander.lerma@neuralnetes.com>
2022-08-10 12:03:46 -0500

    update tf

--- terraform/modules/google/cloud-storage-notifications/main.tf
+++ terraform/modules/google/cloud-storage-notifications/main.tf
@@ -1,10 +1,12 @@
 module "notifications" {
-  for_each          = local.notifications_map
-  source            = "github.com/neuralnetes/monorepo.git//terraform/modules/google/cloud-storage-notification?ref=main"
-  project_id        = each.value["project_id"]
-  bucket_name       = each.value["bucket_name"]
-  pubsub_topic_name = each.value["pubsub_topic_name"]
-  custom_attributes = each.value["custom_attributes"]
+  for_each           = local.notifications_map
+  source             = "github.com/neuralnetes/monorepo.git//terraform/modules/google/cloud-storage-notification?ref=main"
+  project_id         = each.value["project_id"]
+  bucket_name        = each.value["bucket_name"]
+  pubsub_topic_name  = each.value["pubsub_topic_name"]
+  custom_attributes  = each.value["custom_attributes"]
+  object_name_prefix = each.value["object_name_prefix"]
+  event_types        = each.value["event_types"]
 }
 
 locals {
